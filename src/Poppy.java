405a2db765c517ae59eae49dd498bd4d745d0329
Kaiypov Beknazar <koloev.02@gmail.coom>
2023-05-06 18:56:23 +0600

    caMood

---
+++ src/Poppy.java
@@ -0,0 +1,40 @@
+public class Poppy {
+    String name = "Poppy";
+    private int age;
+    private int satiety;
+    private int health;
+    private int mood;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
+}
+
